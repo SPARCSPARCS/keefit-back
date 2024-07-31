7b2a7cab622804d3b37fa5b34622bbda55757891
Hszoo <trre1827151@gmail.com>
2024-07-31 23:22:37 +0900

    CHORE : CORS 허용 도메인 추가

--- src/main/java/com/backend/api/config/WebConfig.java
+++ src/main/java/com/backend/api/config/WebConfig.java
@@ -18,7 +18,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("*", "http://localhost:5173")
 //                .allowedOrigins("http://localhost:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*")
