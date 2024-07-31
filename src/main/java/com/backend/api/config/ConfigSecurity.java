6e14d55898a189b6c460641f19c69562f1197147
Hszoo <trre1827151@gmail.com>
2024-08-01 01:13:06 +0900

    .

--- src/main/java/com/backend/api/config/ConfigSecurity.java
+++ src/main/java/com/backend/api/config/ConfigSecurity.java
@@ -4,19 +4,14 @@ import com.backend.api.filter.DefaultCorsFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.filter.CorsFilter;
-
 @RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
-public class SecurityConfig {
+public class ConfigSecurity {
     private final DefaultCorsFilter defaultCorsFilter;
 
     @Bean
