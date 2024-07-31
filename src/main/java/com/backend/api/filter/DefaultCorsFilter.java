bacb6a9954dded6103920d14442181c10367003b
Hszoo <trre1827151@gmail.com>
2024-08-01 00:44:21 +0900

    CHORE : CORS filter

--- src/main/java/com/backend/api/filter/DefaultCorsFilter.java
+++ src/main/java/com/backend/api/filter/DefaultCorsFilter.java
@@ -15,7 +15,7 @@ public class DefaultCorsFilter extends CorsFilter {
     }
     private static CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:8080"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:8080", "*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Accept", "Authorization", "X-Real-IP"));
         configuration.setAllowCredentials(true);
